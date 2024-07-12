05dd84dc0d15919dbe9aa9b67b06d005d143c039
mdevareddy <mdevareddy77@gmail.com>
2024-07-12 18:21:04 +0530

    teat commit

---
+++ fullname/src/App.js
@@ -0,0 +1,13 @@
+import DisplayFullName from "./DisplayName";
+
+
+function App() {
+  return (
+    <div className="App">
+      <DisplayFullName />
+      
+    </div>
+  );
+}
+
+export default App;
